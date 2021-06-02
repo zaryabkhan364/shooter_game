72633ee8c8472449f7f20e842ab70ed1de40eed8
zaryabkhan364 <khanzaryab007@gmail.com>
2021-06-03 03:02:23 +0500

    make a soldier class

--- shooter.py
+++ shooter.py
@@ -9,23 +9,34 @@ screen_height = int(screen_width * 0.8)
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Shooter')
 
+
+class Soldier(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('assets/img/player/0.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+
 x = 200
 y = 200
-img = pygame.image.load('assets/img/player/0.png')
-rect = img.get_rect()
-rect.center = (x, y)
+scale = 2
+player = Soldier(x, y, scale)
 
 
 run = True
+
 while run:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    screen.blit(img, rect)
+    screen.blit(player.image, player.rect)
 
     pygame.display.update()
     clock.tick(60)
 
-pygame.quit()        
+pygame.quit()
